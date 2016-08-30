69cd3be4b0bcbc3fb9644e691f593d4ade8ecfd7
Vitaly Spirin <vitaly.spirin@gmail.com>
2016-08-30 04:29:47 +0000

    refactoring. moved yii2 from /basic to /vendor

--- tests/setup/yii_init.php
+++ tests/setup/yii_init.php
@@ -5,16 +5,16 @@ error_reporting(-1);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require(__DIR__ . '/../../basic/vendor/yiisoft/yii2/Yii.php');
+require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 
 
 if ( php_sapi_name() === 'cli')
 {
-    $config = require(__DIR__ . '/../../basic/config/console.php');
+    $config = require(__DIR__ . '/config/console.php');
     $application = new yii\console\Application($config);
 } else
 {
-    $yiiConfig = require(__DIR__ . '/../../basic/config/web.php');
+    $yiiConfig = require(__DIR__ . '/config/web.php');
     new yii\web\Application($yiiConfig); // Do NOT call run() here
 }
 
